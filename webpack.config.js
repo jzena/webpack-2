8ec02486ae593e2fa94911a04734dd35e3cf57a0
jose_zena <jose.zena.zamora@gmail.com>
2017-10-19 20:26:34 -0500

    working with multiple html files

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 // https://github.com/howtocodeio/lets-learn-webpack-2
 
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
 module.exports = {
     entry: {
         app: './src/index.js',
@@ -49,7 +51,32 @@ module.exports = {
         new CleanWebpackPlugin(['dist']),
         new ExtractTextPlugin('styles.css'),
         new htmlWebpackPlugin({
-            title: 'Multiple Bundles!'
-        })
+            filename: 'index.html',
+            template: 'src/views/index.html',
+            chunks: ['app']
+        }),
+        new htmlWebpackPlugin({
+            filename: 'about.html',
+            template: 'src/views/about.html',
+            chunks: ['about']
+        }),
+        new BrowserSyncPlugin(
+            // BrowserSync options
+            {
+                // browse to http://localhost:3000/ during development
+                host: 'localhost',
+                port: 3000,
+                // proxy the Webpack Dev Server endpoint
+                // (which should be serving on http://localhost:3100/)
+                // through BrowserSync
+                proxy: 'http://localhost:8080/'
+            },
+            // plugin options
+            {
+                // prevent BrowserSync from reloading the page
+                // and let Webpack Dev Server take care of this
+                reload: false
+            }
+        )
     ]
 };
